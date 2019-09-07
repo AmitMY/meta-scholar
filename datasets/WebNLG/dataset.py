965da15b4acc0a9664d3919e01f6e18d32f2718d
Amit <amitmoryossef@gmail.com>
2019-09-07 18:33:58 +0300

    dataset(WebNLG): adapt to JSONL format

--- datasets/WebNLG/dataset.py
+++ datasets/WebNLG/dataset.py
@@ -4,50 +4,57 @@ import urllib
 from os import path
 from urllib.request import urlopen
 
+import jsonlines
+
 from utils.file_system import makedir
 
 base = path.dirname(path.abspath(__file__))
 
 
-def download(version="2"):
-    if version not in ["2", "2_constrained"]:
-        raise ValueError("Downloading this version is not implemented")
+def download_version(version="2"):
+    header = json.load(open(path.join(base, "header.json")))
+    versions = [v for v in header["versions"] if v["version"] == version]
+    if len(versions) == 0:
+        raise ValueError("Version not found")
 
     versions_dir = path.join(base, "versions")
     makedir(versions_dir)
     version_dir = path.join(versions_dir, version)
     makedir(version_dir)
 
-    header = json.load(open(path.join(base, "header.json")))
-    versions = [v for v in header["versions"] if v["version"] == version]
-    if len(versions) == 0:
-        raise ValueError("Version not found")
+    return download(versions[0], version_dir)
 
-    url = versions[0]["url"].replace("/tree/", "/raw/") + "/json/webnlg_release_v" + version + "_"
 
-    dataset = {}
+def download(version, directory):
+    if version["version"] not in ["2", "2_constrained"]:
+        raise ValueError("Downloading this version is not implemented")
+
+    url = version["url"].replace("/tree/", "/raw/") + "/json/webnlg_release_v" + version["version"] + "_"
+
+    lines = 0
     splits = {}
 
-    for split in ["dev", "test", "train"]:
-        splits[split] = []
-        split_url = url + split + ".json"
+    with jsonlines.open(path.join(directory, "index.jsonl"), mode='w') as writer:
+        for split in ["dev", "test", "train"]:
+            splits[split] = []
+            split_url = url + split + ".json"
 
-        entries = json.loads(urlopen(split_url).read())["entries"]
-        for entry in entries:
-            for id, datum in entry.items():
-                datum_id = split + "_" + id
-                dataset[datum_id] = {
-                    "category": datum["category"],
-                    "graph": datum["modifiedtripleset"],
-                    "texts": [l["lex"] for l in datum["lexicalisations"]]
-                }
-                splits[split].append(datum_id)
+            print("Downloading split:", split)
+            entries = json.loads(urlopen(split_url).read())["entries"]
+            for entry in entries:
+                for id, datum in entry.items():
+                    writer.write({
+                        "id": split + "_" + id,
+                        "category": datum["category"],
+                        "graph": datum["modifiedtripleset"],
+                        "texts": [l["lex"] for l in datum["lexicalisations"]]
+                    })
 
-    json.dump({"data": dataset}, open(path.join(version_dir, "index.json"), "w"))
-    json.dump(splits, open(path.join(version_dir, "split.json"), "w"))
+                    splits[split].append(lines)
+                    lines += 1
 
-    print("Download done")
+    json.dump(splits, open(path.join(directory, "split.json"), "w"))
 
 
 if __name__ == "__main__":
-    download()
+    download_version("2")
